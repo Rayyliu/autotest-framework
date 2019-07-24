5eb960c6aa7f5a2603e2b7028b9e44f069925115
Ray <466088269@qq.com>
2019-07-24 21:20:21 +0800

    新增参数组装功能

--- src/main/java/com/autotest/extension/AutoTestArgumentsProvider.java
+++ src/main/java/com/autotest/extension/AutoTestArgumentsProvider.java
@@ -1,14 +1,21 @@
 package com.autotest.extension;
 
 import com.autotest.annotation.AutoTest;
+import com.autotest.utils.ObjectTypeChange;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.support.AnnotationConsumer;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
+
+import static com.autotest.extension.AutoTestArgumentsProvider.getClassAllField;
+import static com.autotest.extension.ParameterAssemble.getParameters;
+import static com.autotest.extension.TestMethodParameters.getTestMethodParameters;
 
 public  class AutoTestArgumentsProvider implements ArgumentsProvider, AnnotationConsumer<AutoTest> {
 
@@ -22,7 +29,51 @@ public  class AutoTestArgumentsProvider implements ArgumentsProvider, Annotation
     }
 
     @Override
-    public Stream<? extends Arguments>  provideArguments(ExtensionContext context){
+    public Stream<? extends Arguments>  provideArguments(ExtensionContext context) throws Exception {
+        List<Object> parameterValue = getParameters(context,this.file);
+        List<Parame> pars = getTestMethodParameters(context);
+        for(Object ob:parameterValue){
+            Map<String,String >  map = (Map<String, String>) ob;
+            Object[] obs = null;
+            for(int i = 0;i<pars.size();i++){
+                Class  type= pars.get(i).getType();
+                String p = pars.get(i).getName();
+                if(isJavaClass(type)|| type.isEnum()){
+                    obs[i] = ObjectTypeChange.processing(type, map.get(p));
+                }else{
+                    obs[i] =
+                }
+            }
+        }
         return null;
+    }
+
+    public boolean isJavaClass(Class clazz){
+        return clazz!=null && clazz.getClassLoader()==null;
+    }
+}
+
+    public static Object mapToObject(Class clz,Map<String,String> map) throws IllegalAccessException, InstantiationException {
+        Object obj = clz.newInstance();
+        List<Field> fieldList = new ArrayList<>();
+        fieldList = getClassAllField(clz,fieldList);
+
+    }
+
+    public static List<Field> getClassAllField(Class<?> clazz,List<Field> allFields){
+        if(clazz ==null){
+            return null;
+        }
+    Object parent = clazz.getSuperclass();
+        if(parent!=null && !((Class) parent).getName().equals("java.lang.Object")){
+            getClassAllField((Class<?>) parent,allFields);
+        }
+        Field [] fields = clazz.getDeclaredFields();
+        if(fields!= null) {
+            for (int i = 0; i < fields.length; i++) {
+                allFields.add(fields[i]);
+            }
+        }
+        return allFields;
     }
 }
